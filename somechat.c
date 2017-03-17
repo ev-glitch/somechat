0cb90d983a7899996df15978b974a8ad6709cd38
ev-glitch <mail.evalyn@gmail.com>
2017-03-17 06:25:36 -0400

    Changes to client mode processing; beginning network programming

--- somechat.c
+++ somechat.c
@@ -4,12 +4,20 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+// define chat client modes
+#define CLIENT_EXIT 99
+#define CLIENT_CMD 11
+#define CLIENT_CHAT 22
+
 int start_chat()
 {
     ssize_t read = 0;
     FILE* stream = stdin;
     char* line = NULL;
     size_t len = 0;
+// Start networking code here:
+    struct addrinfo* remote_client = NULL;
+    struct addrinfo remote_client = getaddrinfo(const char *node, const char* service, const struct addrinfo* hints, struct addrinfo** res);
 
     do
     {
@@ -54,7 +62,7 @@ int start_chat()
 int main(int argc, char *argv[])
 {
     char input = '\0';
-    // How does a professional programmer begin a new program?
+    // setup standardized options for commandline init.
     // getopt(argc, argv, "options" );
 
     // Stand in variables for networking:
@@ -63,7 +71,7 @@ int main(int argc, char *argv[])
     int port = 5666;
     printf("Connecting to host %s, port %d.\n", host, port);
 
-    char  client_mode    = 'C';
+    char  client_mode    = CLIENT_CMD;
     char* client_prompt = "Command>> ";
     ssize_t client_read = 0;
     char* client_input = NULL;
@@ -76,14 +84,24 @@ int main(int argc, char *argv[])
         printf("%s", client_prompt);
 
         // get new input
-        if( (client_read = getline(&client_input, &client_input_len, chat_stream)) == -1)
-            exit(-1);
+        if( (client_read = getline(&client_input, &client_input_len, chat_stream)) == -1) exit(-1);
+        printf("client_read = \"%s\"\n", client_read);
 
         // Process input according to client_mode
-        if( client_mode == 'C')
+        if( client_mode == CLIENT_CMD)
+        {    
+            // Process a command entry
+            if(str_cmp(client_read, "exit"))    { client_mode = CLIENT_EXIT; } else
+            if(str_cmp(client_read, "chat"))    { client_mode = CLIENT_CHAT; } else
+            if(str_cmp(client_read, "command")) { client_mode = CLIENT_CMD; }
+
+
+        } else if( client_mode == CLIENT_CHAT)
+        {
             start_chat();
             return 0;
-        else if( client_mode == 'T')
+        }
+        else if( client_mode == CLIENT_EXIT)
             return 0;
 
         
